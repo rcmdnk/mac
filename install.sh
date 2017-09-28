ad0c5b0ff0771c0368677a04636a52fcd531cf9d
rcmdnk <rcmdnk@gmail.com>
2017-09-28 17:08:44 +0900

    updated install script

--- install.sh
+++ install.sh
@@ -5,26 +5,26 @@ if [[ ! "$OSTYPE" =~ darwin ]];then
   exit
 fi
 
+files=()
 #files=(karabiner.json)
 #karabiner="$HOME/.config/karabiner"
 instdirs=("$karabiner" "$karabiner")
 exclude=("." ".." "LICENSE" "README.md" "install.sh")
 
-backup="bak"
+backup=""
 overwrite=1
 dryrun=0
 newlink=()
 exist=()
-curdir=`pwd -P`
+curdir=$(pwd -P)
 # help
 HELP="Usage: $0 [-nd] [-b <backup file postfix>] [-e <exclude file>]
 
 Make links of Mac related files/directories
 
 Arguments:
-      -b  Set backup postfix (default: make *.bak file)
-          Set \"\" if backups are not necessary
-      -e  Set additional exclude file (default: ${exclude[@]})
+      -b  Set backup postfix, like \"bak\" (default: \"\": no back up is made)
+      -e  Set additional exclude file (default: ${exclude[*]})
       -n  Don't overwrite if file is already exist
       -d  Dry run, don't install anything
       -h  Print Help (this message) and exit
@@ -48,11 +48,11 @@ if [ $dryrun -ne 0 ];then
   echo "*** This is dry run, not install anything ***"
 fi
 i=0
-while [ $i -lt ${#files[@]} ];do
+while [ $i -lt "${#files[@]}" ];do
   f=${files[$i]}
   d=${instdirs[$i]}
-  i=$(($i+1))
-  for e in ${exclude[@]};do
+  i=$((i+1))
+  for e in "${exclude[@]}";do
     flag=0
     if [ "$f" = "$e" ];then
       flag=1
@@ -62,12 +62,12 @@ while [ $i -lt ${#files[@]} ];do
   if [ $flag = 1 ];then
     continue
   fi
-  echo install $f to \"$d\"
+  echo "install $f to $d"
   install=1
   if [ $dryrun -eq 1 ];then
     install=0
   fi
-  if [ "`ls "$d/$f" 2>/dev/null`" != "" ];then
+  if [ "$(ls "$d/$f" 2>/dev/null)" != "" ];then
     exist=(${exist[@]} "$f")
     if [ $dryrun -eq 1 ];then
       echo -n ""
@@ -94,7 +94,7 @@ if [ $dryrun -eq 1 ];then
 else
   echo "Following files were newly installed:"
 fi
-echo "  ${newlink[@]}"
+echo "  ${newlink[*]}"
 echo
 echo -n "Following files existed"
 if [ $dryrun -eq 1 ];then
@@ -106,5 +106,5 @@ elif [ "$backup" != "" ];then
 else
   echo "Following files existed, replaced old one:"
 fi
-echo "  ${exist[@]}"
+echo "  ${exist[*]}"
 echo
